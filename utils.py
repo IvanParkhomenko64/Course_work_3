699dfcfdf549e046ee16609a60f0e6952fcc0ba8
Ivan Parkhomenko <ippeng54@gmail.com>
2023-03-25 13:34:09 +0300

    add def load_operations

--- utils.py
+++ utils.py
@@ -1,2 +1,11 @@
+import json  #подключение модуля для работы с объектами типа json
 
+
+def load_operations(file_operations):
+    """Загружает список операций из файла"""
+    list_operations = []
+    with open(file_operations, 'r', encoding='utf-8') as f:
+       raw_json = f.read()
+    list_operations = json.loads(raw_json)
+    return list_operations  #выводим список операций
 
