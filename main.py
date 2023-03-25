e8a788308273a8a4818bca1d819c07e74ab04a02
Ivan Parkhomenko <ippeng54@gmail.com>
2023-03-25 18:01:07 +0300

    rework def operation_output

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from utils import load_operations, print_operations
+import datetime
+
 
 file_operations = 'operations.json'
 
 print_operations(load_operations(file_operations))
+
+# dict_ = load_operations(file_operations)[1]
+# print(dict_['date'])
+# date = datetime.datetime.strptime(dict_["date"], "%Y-%m-%dT%H:%M:%S.%f")
+# date_str = date.strftime('%d.%m.%Y')
+# print(type(date_str))
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json  #подключение модуля для работы с объектами типа json
+import datetime
 from operator import itemgetter
 
 def get_date(dictionary):
@@ -14,20 +15,22 @@ def load_operations(file_operations):
     #sorted_list_operations = sorted(list_operations, key=itemgetter('date'))
     return list_operations  #выводим список операций
 
-def operation_output(dict: dict):
-   if 'from' in dict.keys():
-     from_dict = dict['from'].split()
+def operation_output(dict_item: dict):
+   date = datetime.datetime.strptime(dict_item["date"], "%Y-%m-%dT%H:%M:%S.%f")
+   date_str = date.strftime('%d.%m.%Y')
+   if 'from' in dict_item.keys():
+     from_dict = dict_item['from'].split()
      from_dict_str = from_dict[len(from_dict)-1]
      if len(from_dict_str) == 20:
-         str_from = from_dict[0] + ' **' + from_dict_str[len(from_dict_str)-1:len(from_dict_str)-5:-1]
+         str_from = from_dict[0] + ' **' + from_dict_str[len(from_dict_str)-1:len(from_dict_str)-5:-1] + ' --> '
      elif len(from_dict) > 2:
-         str_from = from_dict[0] + ' ' + from_dict[1] + ' ' + from_dict_str[0:4] + ' ' + from_dict_str[5:7] + '** **** '+from_dict_str[len(from_dict_str) - 1:len(from_dict_str) - 5:-1]
+         str_from = from_dict[0] + ' ' + from_dict[1] + ' ' + from_dict_str[0:4] + ' ' + from_dict_str[5:7] + '** **** '+from_dict_str[len(from_dict_str) - 1:len(from_dict_str) - 5:-1] + ' --> '
      else:
-         str_from = from_dict[0] + ' ' + from_dict_str[0:4] + ' ' + from_dict_str[5:7] + '** **** ' + from_dict_str[len(from_dict_str) - 1:len(from_dict_str) - 5:-1]
+         str_from = from_dict[0] + ' ' + from_dict_str[0:4] + ' ' + from_dict_str[5:7] + '** **** ' + from_dict_str[len(from_dict_str) - 1:len(from_dict_str) - 5:-1] + ' --> '
    else:
        str_from = ''
 
-   to_dict = dict['to'].split()
+   to_dict = dict_item['to'].split()
    to_dict_str = to_dict[len(to_dict) - 1]
    if len(to_dict_str) == 20:
        str_to = to_dict[0] + ' **' + to_dict_str[len(to_dict_str)-1:len(to_dict_str)-5:-1]
@@ -36,9 +39,9 @@ def operation_output(dict: dict):
    else:
        str_to = to_dict[0] + ' ' + to_dict_str[0:4] + ' ' + to_dict_str[5:7] + '** **** ' + to_dict_str[len(to_dict_str) - 1:len(to_dict_str) - 5:-1]
 
-   s = dict['date'] + ' ' + dict['description'] + '\n'
-   s = s + str_from + '->' + str_to + '\n'
-   s = s + dict['operationAmount']['amount'] + ' ' + dict['operationAmount']['currency']['name'] + '\n'
+   s = date_str + ' ' + dict_item['description'] + '\n'
+   s = s + str_from + str_to + '\n'
+   s = s + dict_item['operationAmount']['amount'] + ' ' + dict_item['operationAmount']['currency']['name'] + '\n'
    return s
 
 def print_operations(list_operations):
